af91e68c2e0ffae7641a2a9ac14ae8efe715277f
Raymoun17 <raymoun17@gmail.com>
2020-06-03 18:31:59 -0400

    auto commit

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ async def on_message(message):
 
     check = check_stupid(message.content, ["gay"])
     if check:
-        await message.channel.send('no u!')
+        await bot.process_commands(message)
 
 
 bot.run(token)
