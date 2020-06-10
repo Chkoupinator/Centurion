f362e6948b27af58e6725893f046ffb9738149b4
Raymoun17 <raymoun17@gmail.com>
2020-06-10 03:39:40 -0400

    auto commit

--- functions.py
+++ functions.py
@@ -115,10 +115,15 @@ def get_dad_joke():
 
 def check_stupid(message, word_list):
 
-    while message[0] == "*" or message[0] == " ":
-        message = message[1:]
-    while message[-1] == "*" or message[-1] == " ":
-        message = message[:-1]
+    try:
+        message.replace("*", "")
+        message.replace(" ", "")
+        message.replace(".", "")
+        message.replace("!", "")
+        message.replace("?", "")
+    except:
+        pass
+
 
     message = message.split()
     res = False
